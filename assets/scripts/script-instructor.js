4d8c27edc0e5c92441da6abc30d81e0386be2acc
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-09-12 22:43:58 -0400

    Implemented rock paper scissors game with the instructor

--- assets/scripts/script-instructor.js
+++ assets/scripts/script-instructor.js
@@ -2,9 +2,34 @@ var choices = ["r", "p", "s"];
 var wins = 0;
 var losses = 0;
 var ties = 0;
+var keepPlaying = true;
 
-var humanChoice = prompt("Choose r, p, s");
-console.log(humanChoice);
+while (keepPlaying) {
+    var humanChoice = prompt("Choose r, p, s");
+    // console.log(humanChoice);
 
-var computerChoice = choices[Math.floor(3 * Math.random())];
-console.log(computerChoice);
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // console.log(computerChoice);
+
+    var result;
+    if (humanChoice === computerChoice) {
+        ties++;
+        result = "tied!";
+    } else if (
+        humanChoice === "r" && computerChoice === "s" ||
+        humanChoice === "p" && computerChoice === "r" ||
+        humanChoice === "s" && computerChoice === "p"
+        ) {
+        // Human Wins
+        wins++;
+        result = "won!"
+    } else {
+        // Computer Wins
+        losses++;
+        result = "lost!";
+    }
+
+    alert("You " + result);
+
+    alert("Stats:")
+}
